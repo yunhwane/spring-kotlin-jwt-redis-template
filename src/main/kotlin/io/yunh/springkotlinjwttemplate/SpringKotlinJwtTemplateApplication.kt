c162ad9c9105c9c3443b15c2f705114811f88002
yunhwan <147581818+yunhwane@users.noreply.github.com>
2025-06-24 17:12:40 +0900

    initial commit

---
+++ src/main/kotlin/io/yunh/springkotlinjwttemplate/SpringKotlinJwtTemplateApplication.kt
@@ -0,0 +1,11 @@
+package io.yunh.springkotlinjwttemplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinJwtTemplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinJwtTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/io/yunh/springkotlinjwttemplate/SpringKotlinJwtTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.yunh.springkotlinjwttemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinJwtTemplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
